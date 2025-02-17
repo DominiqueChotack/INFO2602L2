9809ddf2fcab169b692d4df30d6e3e3521ec6aa2
dchotack <28389731-dchotack@users.noreply.replit.com>
2025-02-17 22:27:08 +0000

    User checkpoint: Add Todo model and CLI commands for managing user tasks.  Includes functionality to add, retrieve, and manage todos for a given user.

--- wsgi.py
+++ wsgi.py
@@ -10,5 +10,70 @@ def initialize():
   db.init_app(app)
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
+  bob.todos.append(Todo('wash car'))
+  db.session.add(bob)
+  db.session.commit()
   print(bob)
   print('database intialized')
+
+@app.cli.command("get-user", help="Retrieves a User")
+@click.argument('username', default='bob')
+def get_user(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print(f'{username} not found!')
+    return
+  print(bob)
+
+@app.cli.command('get-users')
+def get_users():
+  # gets all objects of a model
+  users = User.query.all()
+  print(users)
+
+@app.cli.command("change-email")
+@click.argument('username', default='bob')
+@click.argument('email', default='bob@mail.com')
+def change_email(username, email):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  bob.email = email
+  db.session.add(bob)
+  db.session.commit()
+  print(bob)
+
+@app.cli.command('delete-user')
+@click.argument('username', default='bob')
+def delete_user(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  db.session.delete(bob)
+  db.session.commit()
+  print(f'{username} deleted')
+
+@app.cli.command('get-todos')
+@click.argument('username', default='bob')
+def get_user_todos(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  print(bob.todos)
+
+@app.cli.command('add-todo')
+@click.argument('username', default='bob')
+@click.argument('text', default='wash car')
+def add_task(username, text):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  new_todo = Todo(text)
+  bob.todos.append(new_todo)
+  db.session.add(bob)
+  db.session.commit()
+  
