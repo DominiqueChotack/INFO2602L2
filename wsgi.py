6fb22445330946789235a4140176209e29db17bc
dchotack <28389731-dchotack@users.noreply.replit.com>
2025-02-17 22:35:08 +0000

    User checkpoint: Add TodoCategory model and CLI command to toggle todo status.  Enhance User and Todo models with category functionality and improved representations.

--- wsgi.py
+++ wsgi.py
@@ -76,4 +76,19 @@ def add_task(username, text):
   new_todo.user_id = bob.id
   db.session.add(new_todo)
   db.session.commit()
-  
+
+@click.argument('todo_id', default=1)
+@click.argument('username', default='bob')
+@app.cli.command('toggle-todo')
+def toggle_todo_command(todo_id, username):
+  user = User.query.filter_by(username=username).first()
+  if not user:
+    print(f'{username} not found!')
+    return
+
+  todo = Todo.query.filter_by(id=todo_id, user_id=user.id).first()
+  if not todo:
+    print(f'{username} has no todo id {todo_id}')
+
+  todo.toggle()
+  print(f'{todo.text} is {"done" if todo.done else "not done"}!')
